039fdcf752f72761536e93cfd6c4b4661d94590d
YUVA-2329 <yuvakishore.vps@gmail.com>
2025-11-20 22:14:15 +0530

    Add files via upload

---
+++ PYTHON ASSIGNMENT/PROGRAM 2.py
@@ -0,0 +1,5 @@
+'''2.An App for Mobile hosting provider charges- $0.51 per hour . 
+ how much does it cost to operator per day, per week, per month? 
+how many days can i operate one server with$918?'''
+cost=0.51
+  
